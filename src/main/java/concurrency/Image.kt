f388189c1ee1c705cefcae72aa9e9fd6d5dea5c1
Nick Cruz <ncruz@umich.edu>
2017-09-30 13:04:07 -0500

    Prepared classes for scheduler injection and wrote virtual time test.

--- src/main/java/concurrency/Image.kt
+++ src/main/java/concurrency/Image.kt
@@ -3,6 +3,7 @@
 package concurrency
 
 import io.reactivex.Observable
+import io.reactivex.Scheduler
 import io.reactivex.Single
 import io.reactivex.schedulers.Schedulers
 import java.util.concurrent.TimeUnit
@@ -13,17 +14,18 @@ interface ImageRepository {
     fun downloadImageById(imageId: Int): Single<Image>
 }
 
-class ImageDownloader(private val imageRepository: ImageRepository) {
+class ImageDownloader(private val imageRepository: ImageRepository,
+                      private val ioScheduler: Scheduler = Schedulers.io()) {
     fun downloadImages() : Single<List<Image>> = Observable.fromArray(1, 2, 3, 4, 5)
             .flatMap { imageRepository.downloadImageById(it)
-                    .subscribeOn(Schedulers.io())
+                    .subscribeOn(ioScheduler)
                     .toObservable() }
             .toList()
 }
 
-class DelayedRepository : ImageRepository {
+class DelayedRepository(private val delayScheduler: Scheduler = Schedulers.computation()) : ImageRepository {
     override fun downloadImageById(imageId: Int): Single<Image> {
         return Single.just(Image(imageId))
-                .delay(5, TimeUnit.SECONDS)
+                .delay(5, TimeUnit.SECONDS, delayScheduler)
     }
 }
--- src/test/java/concurrency/DownloaderTest.kt
+++ src/test/java/concurrency/DownloaderTest.kt
@@ -1,7 +1,9 @@
 package concurrency
 
+import io.reactivex.schedulers.TestScheduler
 import org.junit.Assert.assertTrue
 import org.junit.Test
+import java.util.concurrent.TimeUnit
 import kotlin.system.measureTimeMillis
 
 /**
@@ -31,6 +33,26 @@ class DownloaderTest {
 
     @Test
     fun concurrentDownload_IsInstant() {
+        val testScheduler = TestScheduler()
+        imageDownloader = ImageDownloader(DelayedRepository(testScheduler), testScheduler)
 
+        val actualList = mutableListOf<Image>()
+
+        imageDownloader.downloadImages()
+                .subscribe { list -> actualList.addAll(list)}
+
+        // After 4 seconds, we can assert that our list is still empty.
+        testScheduler.advanceTimeBy(4, TimeUnit.SECONDS)
+        assertTrue(actualList.isEmpty())
+
+        // After a 5th second, we can assert that our list is loaded.
+        testScheduler.advanceTimeBy(1, TimeUnit.SECONDS)
+        assertTrue(actualList.containsAll(setOf(
+                Image(1),
+                Image(2),
+                Image(3),
+                Image(4),
+                Image(5)
+        )))
     }
 }
